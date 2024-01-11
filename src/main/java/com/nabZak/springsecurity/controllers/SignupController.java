b650d9750b6469dead07318ccddd87b62c5dcadd
Zaki Nabil <liban1@gmx.com>
2024-01-11 13:31:04 +0100

    WebsecurityConfig->FilterChain;

--- src/main/java/com/nabZak/springsecurity/controllers/SignupController.java
+++ src/main/java/com/nabZak/springsecurity/controllers/SignupController.java
@@ -2,21 +2,30 @@ package com.nabZak.springsecurity.controllers;
 
 import com.nabZak.springsecurity.dto.SignupRequest;
 import com.nabZak.springsecurity.services.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/singup")
+@RequestMapping("/signup")
 public class SignupController {
     private final AuthService authService;
-
-    public AuthServiceImpl(AuthService authService) {
+    @Autowired
+    public SignupController(AuthService authService) {
         this.authService = authService;
     }
-
+    @PostMapping
     public ResponseEntity<String> signupCustomer(@RequestBody SignupRequest signupRequest){
         boolean isUserCreated = authService.createCustomer(signupRequest);
+        if (isUserCreated){
+            return  ResponseEntity.status(HttpStatus.CREATED).body("Customer created succesfully");
+        } else {
+            return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to create customer");
+        }
     }
 }
--- src/main/java/com/nabZak/springsecurity/services/AuthServiceImpl.java
+++ src/main/java/com/nabZak/springsecurity/services/AuthServiceImpl.java
@@ -1,7 +1,10 @@
 package com.nabZak.springsecurity.services;
 
 import com.nabZak.springsecurity.dto.SignupRequest;
+import com.nabZak.springsecurity.entities.Customer;
 import com.nabZak.springsecurity.repository.CustomerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +14,7 @@ public class AuthServiceImpl implements AuthService{
     private final CustomerRepository customerRepository;
 
     private final PasswordEncoder passwordEncoder;
-
+    @Autowired
     public AuthServiceImpl(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
         this.customerRepository = customerRepository;
         this.passwordEncoder = passwordEncoder;
@@ -19,6 +22,17 @@ public class AuthServiceImpl implements AuthService{
 
     @Override
     public boolean createCustomer(SignupRequest signupRequest) {
-        return false;
+        //Check if customer already exist
+        if (customerRepository.existsByEmail(signupRequest.getEmail())){
+            return false;
+        }
+        Customer customer = new Customer();
+        BeanUtils.copyProperties(signupRequest, customer);
+
+        //Hash the password before saving
+        String hashPassword = passwordEncoder.encode(signupRequest.getPassword());
+        customer.setPassword(hashPassword);
+        customerRepository.save(customer);
+        return true;
     }
 }
